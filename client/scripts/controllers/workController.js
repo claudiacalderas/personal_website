e5f4c1a80e588d6ca184fafb92d19206497d8d79
Claudia Calderas <claudia.calderas@gmail.com>
2017-06-24 16:26:42 -0500

    added cards to show portfolio items

--- client/scripts/controllers/workController.js
+++ client/scripts/controllers/workController.js
@@ -2,5 +2,12 @@ myApp.controller('WorkController', ['$scope', '$location', '$interval', '$window
 
   console.log('WorkController loaded');
 
+  $scope.portfolioItems = [
+    {title: 'Otomi',
+    image_url: 'views/images/ShopPhoto1.jpg'},
+    {title: 'Pecora Madrid',
+    image_url: 'views/images/ShopPhoto2.jpg'}
+  ];
+
 
 }]);
