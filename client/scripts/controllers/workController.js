a4064083a84913e69eac9e97923c6edac0e24717
Claudia Calderas <claudia.calderas@gmail.com>
2017-06-30 11:20:46 -0500

    working on multiple photos for a specific theme

--- client/scripts/controllers/workController.js
+++ client/scripts/controllers/workController.js
@@ -7,9 +7,19 @@ myApp.controller('WorkController', ['$scope', '$location', '$interval', '$window
   $scope.portfolioItems = [
     {title: 'Otomi',
     image_url: 'views/images/ShopPhoto1.jpg',
+    immages: ['views/images/ShopPhoto1.jpg',
+              'views/images/ShopPhoto2.jpg',
+              'views/images/ShopPhoto3.jpg',
+              'views/images/ShopPhoto4.jpg',
+              'views/images/ShopPhoto5.jpg'],
     description: 'Series of Otomi inspired small paintings'},
     {title: 'Pecora Madrid',
     image_url: 'views/images/ShopPhoto2.jpg',
+    immages: ['views/images/ShopPhoto5.jpg',
+              'views/images/ShopPhoto4.jpg',
+              'views/images/ShopPhoto3.jpg',
+              'views/images/ShopPhoto2.jpg',
+              'views/images/ShopPhoto1.jpg'],
     description: 'Logo redesign and stationary'}
   ];
 
