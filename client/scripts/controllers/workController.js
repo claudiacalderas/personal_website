def6f87342c05b7678414585d658e0da5bfc1738
Claudia Calderas <claudia.calderas@gmail.com>
2017-07-02 20:25:26 -0500

    adding content to portfolio

--- client/scripts/controllers/workController.js
+++ client/scripts/controllers/workController.js
@@ -15,12 +15,13 @@ myApp.controller('WorkController', ['$scope', '$location', '$interval', '$window
     description: 'Series of Otomi inspired small paintings'},
     {title: 'Pecora Madrid',
     image_url: 'views/images/ShopPhoto2.jpg',
-    images: ['views/images/ShopPhoto5.jpg',
-              'views/images/ShopPhoto4.jpg',
-              'views/images/ShopPhoto3.jpg',
+    images: ['https://s3.amazonaws.com/artwork-photos/Pecora_Logo1.png',
+              'https://s3.amazonaws.com/artwork-photos/Pecora_Logo2.png',
+              'https://s3.amazonaws.com/artwork-photos/Pecora_Flowers1.png',
               'views/images/ShopPhoto2.jpg',
               'views/images/ShopPhoto1.jpg'],
-    description: 'Logo redesign and stationary'}
+    description: 'Logo redesign and stationary',
+    long_description: 'Logo redesign and stationary for Handmade Jewelry Shop, Pecora Madrid. '}
   ];
 
   $scope.goToSpecificItem = function(item) {
