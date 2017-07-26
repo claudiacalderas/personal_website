7bf807f614b58938f743ecc4daf3e09ffec54c9c
Claudia Calderas <claudia.calderas@gmail.com>
2017-07-26 10:32:07 -0500

    db configuration

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,5 +1,17 @@
 var mongoose = require('mongoose');
-var mongoURI = 'mongodb://localhost:27017/personalwebpage';
+
+// Mongo Connection //
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+  // use the string value of the environment variable
+  mongoURI = process.env.MONGODB_URI;
+} else {
+  // use the local database server
+  mongoURI = 'mongodb://localhost:27017/personalwebpage';
+}
+
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on('error', function(err){
